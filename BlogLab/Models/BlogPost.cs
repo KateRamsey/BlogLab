8c53e4e472b35906d99687551142b7422458a2f7
Kate Ramsey <kateramsey@live.com>
2016-04-07 14:51:16 -0500

    Add BlogPost model

---
+++ BlogLab/Models/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogLab.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public string Author { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
