7afe1ff65efedcf3aeb54974eac9489723bda24b
Kate Ramsey <kateramsey@live.com>
2016-04-11 18:08:00 -0500

    Wrote seed function if there are no blog posts

--- BlogLab/Migrations/Configuration.cs
+++ BlogLab/Migrations/Configuration.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using BlogLab.Models;
+using FizzWare.NBuilder;
+
 namespace BlogLab.Migrations
 {
     using System;
@@ -15,18 +19,22 @@ namespace BlogLab.Migrations
 
         protected override void Seed(BlogDBContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            if (!context.BlogPosts.Any())
+            {
+                List<BlogPost> posts = (List<BlogPost>)Builder<BlogPost>.CreateListOfSize(10)
+                    .All().With(a => a.Author = Faker.NameFaker.Name())
+                    .With(x=>x.CreateDate = Faker.DateTimeFaker.DateTime(DateTime.Now.AddMonths(-4), DateTime.Now))
+                    .With(b=>b.Body = Faker.TextFaker.Sentences(30))
+                    .With(t=>t.Title = Faker.StringFaker.Alpha(30))
+                    .With(p=>p.ImageLink = "http://i.imgur.com/eQjT6JF.jpg")
+                    .Random(4).With(p => p.ImageLink = "http://i.imgur.com/0WHwXdl.jpg")
+                    .Random(4).With(p => p.ImageLink = "http://i.imgur.com/yecyFDs.jpg")
+                    .Random(4).With(p => p.ImageLink = "http://i.imgur.com/iH3eNxZ.jpg")
+                    .Build();
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                context.BlogPosts.AddRange(posts);
+                context.SaveChanges();
+            }
         }
     }
 }
